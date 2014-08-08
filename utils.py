031fe0f56ca7fedeee5f8ac4f2fa69cfe34bce31
Sharath Gururaj <sgururaj@coverity.com>
2014-08-08 11:30:57 -0700

    changes

--- utils.py
+++ utils.py
@@ -1,20 +1,43 @@
 __author__ = 'sgururaj'
 
+log_config = {
+    'version': 1,
+    'formatters': {
+        'myformatter': {
+            'format': "[PYTHON EXEC] %(name)s %(message)s"
+        }
+    },
+    'handlers': {
+        'myhandler': {
+            'formatter': 'myformatter',
+            'class': 'logging.StreamHandler'
+        }
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['myhandler']
+    }
+}
 import subprocess
+import logging
+import logging.config
+logging.config.dictConfig(log_config)
+log = logging.getLogger(__name__)
 
+class Proc(subprocess.Popen):
+    def __init__(self, *args, **kwargs):
+        log.debug(" ".join(args[0]))
+        self._stdout = kwargs.get('stdout', None)
+        super(Proc, self).__init__(*args, **kwargs)
 
-def call(cmd):
-    print ("[ PYTHON_EXEC ]" + " ".join(cmd))
-    process = subprocess.Popen(cmd, bufsize=-1)
-    ret = process.wait()
-    if ret != 0:
-        raise subprocess.CalledProcessError(ret, cmd)
+    def checked_call(self):
+        ret = self.wait()
+        if ret != 0:
+            raise subprocess.CalledProcessError(ret, " ".join(self.args))
 
-
-
-def call_capture(cmd):
-    print (" ".join(cmd))
-    process = subprocess.Popen(cmd, bufsize=-1, stdout=subprocess.PIPE)
-    for line in process.stdout.readline():
-        yield line.decode("utf8")
-
+    def capture_output(self):
+        if self._stdout != subprocess.PIPE:
+            self.kill()
+            raise subprocess.CalledProcessError(self.returncode, " ".join(self.args))
+        for line in self.stdout:
+            yield line.decode("utf8")
